dbe573542e07367d63a1bba93c84d32b0585c69c
Brayan <job.brquirino@gmail.com>
2023-12-21 18:46:51 -0300

    Removing some use client configs were incorrected

--- src/components/Comments/Form/index.tsx
+++ src/components/Comments/Form/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
@@ -7,6 +9,7 @@ import { Input } from "@/components/ui/input";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 interface IFormInput {
+  name: string;
   comment: string;
 }
 
@@ -18,18 +21,29 @@ const CommentForm = () => {
     watch,
     formState: { errors },
   } = useForm<IFormInput>();
+
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="mt-6">
       <div className="grid w-full gap-2">
-        <Input type="Name" placeholder="Type your name" />
+        <Input
+          type="Name"
+          placeholder="Type your name"
+          {...register("name", {
+            maxLength: {
+              value: 20,
+              message: "This input exceed maxLength. The limit is 20 chars",
+            },
+          })}
+        />
         <Textarea
           {...register("comment", {
             maxLength: {
               value: TEXTAREA_LIMIT,
               message: "This input exceed maxLength. The limit is 200 chars",
             },
+            required: true,
           })}
           placeholder="Type your message here."
           className="resize-none outline-none h-40"
@@ -37,6 +51,7 @@ const CommentForm = () => {
         <p className="w-full text-end text-sm font-medium">
           {watch("comment") ? watch("comment").length : 0} / {TEXTAREA_LIMIT}
         </p>
+
         {errors.comment && (
           <p className="text-sm font-medium text-red-500">
             {errors.comment.message}
--- src/components/Knowledges/index.tsx
+++ src/components/Knowledges/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { Progress } from "../ui/progress";
 import { KnowledgesItem } from "./types";

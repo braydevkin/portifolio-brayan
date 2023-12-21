8078815f4cf7e07b99ba6a599471d4d2918ab284
Brayan <job.brquirino@gmail.com>
2023-12-21 17:23:02 -0300

    Adding responsive knowledges page session

---
+++ src/components/Knowledges/knowledges.ts
@@ -0,0 +1,13 @@
+import { KnowledgesItem } from "./types";
+
+export const KNOWLEDGES: KnowledgesItem[] = [
+  { title: "Javascript", percentageOfDominance: 80 },
+  { title: "Typescript", percentageOfDominance: 80 },
+  { title: "NodeJS", percentageOfDominance: 70 },
+  { title: "React", percentageOfDominance: 85 },
+  { title: "No-SQL", percentageOfDominance: 90 },
+  { title: "SQL", percentageOfDominance: 70 },
+  { title: "Cloud", percentageOfDominance: 70 },
+  { title: "Infrastructure & Data", percentageOfDominance: 70 },
+  { title: "HTML & CSS", percentageOfDominance: 90 },
+];

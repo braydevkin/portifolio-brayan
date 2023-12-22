1bc1a59b7f1023e75992f432eb48f65f7f604d10
Brayan <job.brquirino@gmail.com>
2023-12-22 13:46:05 -0300

    Adding created and updated information on model schema and docker compose adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 
-import PageLayout from "@/layout/PagesLayout";
-
 import AboutMe from "@/components/AboutMe";
 
 import Projects from "@/components/Projects";
-import { PROJECTS_HISTORIC } from "@/components/Projects/projects-historic";
-import Knowledges from "@/components/Knowledges";
-import { KNOWLEDGES } from "@/components/Knowledges/knowledges";
 import Comments from "@/components/Comments";
+import Knowledges from "@/components/Knowledges";
 
-const Home = () => {
+import { PROJECTS_HISTORIC } from "@/components/Projects/projects-historic";
+import { KNOWLEDGES } from "@/components/Knowledges/knowledges";
+import { listComments } from "@/server/services/comment";
+
+const Home = async () => {
+  const { comments } = await listComments({ skip: 1, take: 10 });
   return (
     <>
       <AboutMe />
       <Projects historic={PROJECTS_HISTORIC} />
       <Knowledges knowledges={KNOWLEDGES} />
-      <Comments comments={[]} />
+      <Comments comments={comments} />
     </>
   );
 };
--- src/server/services/comment.ts
+++ src/server/services/comment.ts
@@ -1,6 +1,29 @@
 import { Comment } from "@prisma/client";
 import { prismaClient } from "../PrismaClientConfig";
 
+export type ListComments = {
+  skip: number;
+  take: number;
+};
+
+export type ListCommentsResponse = {
+  total: number;
+  comments: Comment[];
+};
+
 export const createComment = async (data: Comment): Promise<Comment> => {
   return await prismaClient.comment.create({ data });
+};
+
+export const listComments = async ({
+  skip,
+  take,
+}: ListComments): Promise<ListCommentsResponse> => {
+  const comments = await prismaClient.comment.findMany({ skip, take });
+  const total = await prismaClient.comment.count();
+
+  return {
+    total,
+    comments,
+  };
 };
